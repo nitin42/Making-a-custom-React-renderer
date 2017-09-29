e5e890ffd0af3e42a01cad6174d9eceb582ea62f
Nitin <tulswani19@gmail.com>
2017-09-29 15:20:41 +0530

    added test renderer

---
+++ src/testRenderer.js
@@ -0,0 +1,13 @@
+import createElement from './utils/createElement';
+import WordRenderer from './reconciler';
+
+function render(element) {
+  const container = createElement('ROOT');
+  const node = WordRenderer.createContainer(container);
+
+  WordRenderer.updateContainer(element, node, null);
+
+  return container;
+}
+
+export default render;
