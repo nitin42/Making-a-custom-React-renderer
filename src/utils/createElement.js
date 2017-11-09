bae8b807225ba2bcd2aec9090d69051c4ea95da3
Nitin <tulswani19@gmail.com>
2017-11-09 09:30:19 +0530

    remove default export

--- src/utils/createElement.js
+++ src/utils/createElement.js
@@ -1,15 +1,30 @@
 import { Document, Text, WordDocument } from '../components/';
 
+let ROOT_NODE_INSTANCE = null
+
+function getHostContextNode(rootNode) {
+  if (typeof rootNode !== undefined) {
+    return ROOT_NODE_INSTANCE = rootNode
+  } else {
+    console.warn(`${rootNode} is not an instance of officegen docx constructor.`)    
+    
+    return ROOT_NODE_INSTANCE = new WordDocument()
+  }
+}
+
 // Creates an element with an element type, props and a root instance
-function createElement(type, props, root) {
+function createElement(type, props) {
   const COMPONENTS = {
     ROOT: () => new WordDocument(),
-    TEXT: () => new Text(root, props),
-    DOCUMENT: () => new Document(root, props),
+    TEXT: () => new Text(ROOT_NODE_INSTANCE, props),
+    DOCUMENT: () => new Document(ROOT_NODE_INSTANCE, props),
     default: undefined,
   };
 
   return COMPONENTS[type]() || COMPONENTS.default;
 }
 
-export default createElement;
+export {
+  createElement,
+  getHostContextNode,
+}
