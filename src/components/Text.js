cf50d336e344a917d7963ab35c8b2eca3e7cc254
Nitin Tulswani <tulswani19@gmail.com>
2017-11-18 14:17:04 +0530

    use .splice()

--- src/components/Text.js
+++ src/components/Text.js
@@ -14,7 +14,7 @@ class Text {
 
   removeChild(child) {
     const index = this.children.indexOf(child);
-    this.children.slice(index, 1);
+    this.children.splice(index, 1);
   }
 
   renderChildren() {
